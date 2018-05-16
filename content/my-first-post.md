22839aa594bf77a69dafe3d9263f19150b487eca
Ryan Mulvey <rem2157+github@columbia.edu>
2018-05-15 23:18:54 -0400

    initial commit

---
+++ content/my-first-post.md
@@ -0,0 +1,10 @@
+Title: My first post
+Date: 2018-05-15 22:03
+Modified: 2018-05-15 22:03
+Category: misc
+Tags: first, misc
+Slug: My-first-post
+Authors: Ryan Mulvey
+Summary: Short version of my first blog post
+
+This is my **first blog post with pelican**
---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ryan Mulvey'
+SITENAME = 'Ryan Mulvey'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Themes
+
+THEME = '../pelican-themes/pelican-bootstrap3'
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+BOOTSTRAP_THEME = 'darkly'
+
+# Plugins
+
+PLUGIN_PATHS = ['../pelican-plugins'] 
+PLUGINS = ['i18n_subsites']
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
